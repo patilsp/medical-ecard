0f05d09fa083cf1367cae4d13d0033458be7c0a3
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2024-07-06 12:57:36 +0530

    bug fixed changes

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
   return (
     <div className="flex h-screen max-h-screen">
       {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[496px]">
           <Image
@@ -24,7 +25,7 @@ const Home = ({ searchParams }: SearchParamProps) => {
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
-              © 2024 Medical E-Card
+              © 2024 CarePluse
             </p>
             <Link href="/?admin=true" className="text-green-500">
               Admin
--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -60,6 +60,7 @@ export const registerPatient = async ({
   ...patient
 }: RegisterUserParams) => {
   try {
+    // Upload file ->  // https://appwrite.io/docs/references/cloud/client-web/storage#createFile
     let file;
     if (identificationDocument) {
       const inputFile =
@@ -72,6 +73,7 @@ export const registerPatient = async ({
       file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
     }
 
+    // Create new patient document -> https://appwrite.io/docs/references/cloud/server-nodejs/databases#createDocument
     const newPatient = await databases.createDocument(
       DATABASE_ID!,
       PATIENT_COLLECTION_ID!,
